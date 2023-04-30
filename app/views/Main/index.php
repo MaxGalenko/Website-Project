2e3c26f7e1bb2bcbe84f0e13f69fdbd2783ecc45
Maxym Galenko <maxymgalenko@gmail.com>
2023-04-30 14:00:22 -0400

    Worked on the main page(displaying rows of products missing)

--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -1,20 +1,27 @@
 <?php $this->view('shared/header', 'Main Page'); ?>
 
+<link rel="stylesheet" type="text/css" href="/css/MainStyle.css">
+
 <h1 style="text-align: center;">Main feed</h1>
 
 <!-- Show product list -->
-<?php foreach ($data as $product): ?>
-  <div>
-    <h2><?php echo $product->title ?></h2>
-    <p><?php echo $product->type ?></p>
-    <p><?php echo $product->description ?></p>
-
-    <?php if ($product->image): ?>
-      <img src="<?php echo "/images/{$product->image}" ?>" alt="Product Image">
-    <?php endif; ?>
-
-    <p><?php echo $product->unit_price ?></p>
-  </div>
-<?php endforeach; ?>
+<?php foreach ($data as $product) { ?>
+	<main>
+	<div class="card">
+		<div class="image">
+			<?php if($product->image){ ?>
+				<img src="/images/<?= $product->image ?>" alt="Product Image">
+			<?php } ?>
+			
+		</div>
+		<div class="caption">
+			<p class="product_name"><?= $product->title ?></p>
+			<p class="price"><b><?= $product->unit_price ?></b></p>
+			<p class="discount"><b><del><?= $product->unit_price ?></del></b></p>
+		</div>
+		<button class="add">Add to cart</button>
+	</div>
+</main>
+<?php } ?>
 
 <?php $this->view('shared/footer'); ?>
