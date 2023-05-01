a4ad5cdb105dbfb9948df7942266e569c9bb0239
Mert Kairstan Salvador <2179970@edu.vaniercollege.qc.ca>
2023-05-01 11:52:11 -0400

    added documentation on the profile indexc

--- app/views/Profile/index.php
+++ app/views/Profile/index.php
@@ -1,27 +1,38 @@
 <?php $this->view('shared/header', 'Profile Page'); ?>
 
-<div>
-    <form method="post" action="/Profile/editProfileInfo">
-        <button type="submit">
-            edit
-        </button>
-    </form>
-    <div>
-        <h1>First Name: </h1>
-        <p><?php echo $data->first_name ?></p>
+<link rel="stylesheet" href="css/Profile/main.css">
 
-        <h1>Middle Name:</h1>
-        <p><?php echo $data->middle_name ?></p>
+<!-- This is the container to preview both address and profile side by side -->
+<div id="personalInfoAddressContainer">
 
-        <h1>Last Name:</h1>
-        <p><?php echo $data->last_name ?></p>
+    <!-- Personal Info Content Container -->
+    <div class="ContentContainers">
+        <!-- This is the button to move to the Editable content file -->
+        <form method="post" action="/Profile/editProfileInfo">
+            <button type="submit"> edit </button>
+        </form>
 
-        <h1>Email:</h1>
-        <p><?php echo $data->email ?></p>
+        <!-- This is the preview information content -->
+        <div>
+            <h1>First Name: </h1>
+            <p><?php echo $data->first_name ?></p>
 
-        <h1>Phone Number:</h1>
-        <p><?php echo $data->phone_number ?></p>
+            <h1>Middle Name:</h1>
+            <p><?php echo $data->middle_name ?></p>
+
+            <h1>Last Name:</h1>
+            <p><?php echo $data->last_name ?></p>
+
+            <h1>Email:</h1>
+            <p><?php echo $data->email ?></p>
+
+            <h1>Phone Number:</h1>
+            <p><?php echo $data->phone_number ?></p>
+        </div>
     </div>
+    
+    <!-- Address Info Content Container -->
+
 </div>
 
 <?php $this->view('shared/footer'); ?>
