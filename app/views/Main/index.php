06168e577bfc961ff70ebf72ba5a688c25474870
Aliraza1002 <Aliraza_159@outlook.com>
2023-05-02 18:15:34 -0400

    working on main page formatting

--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -7,18 +7,10 @@
 <!-- Show product list -->
 <div class="row row-cols-1 row-cols-md-5">
 <?php
-foreach ($data as $product) { ?>
-	<div class="card" style="width: 18rem;">
-	  <img src="..." class="card-img-top" alt="No picture for this product">
-	  <div class="card-body">
-	    <h5 class="card-title">Card title</h5>
-	    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-	    <a href="#" class="btn btn-primary">Go somewhere</a>
-	  </div>
-	</div>
-<?php
-}
-?>
+foreach ($data as $product) {
+	$this->view('Product/partial', $product);
+} ?>
+
 </div>
 
 <!-- 
--- app/views/Product/partial.php
+++ app/views/Product/partial.php
@@ -1,11 +1,25 @@
 <main>
-	<!-- <div class="card">
+	<div class="card" style="width: 18rem;">
+	  <div class="image">
+			<?php if($data->image){ ?>
+				<img src="/images/<?= $data->image ?>" alt="No product Image" style="width: 80%">
+			<?php } ?>
+		</div>
+	  <div class="card-body">
+	    <h5 class="card-title"><?= $data->title ?></h5>
+	    <p class="card-text"><?= $data->unit_price ?></p>
+	    <p class="card-text">Q: <?= $data->quantity ?></p>
+	    <a href="#" class="btn btn-primary">Add to Cart</a>
+	    <a href="#" class="btn btn-primary">View details</a>
+	  </div>
+	</div>
+<!-- <div class="card">
 		<div class="image">
 			<?php if($data->image){ ?>
 				<img src="/images/<?= $data->image ?>" alt="Product Image">
 			<?php } ?>
-		</div> -->
-		<!-- <div class="caption">
+		</div>
+		<div class="caption">
 			<h6 class="product_name"><?= $data->title ?></h6>
 			<p class="price"><b><?= $data->unit_price ?></b></p>
 			<?php if ($data->discount_price != 0) { ?>
@@ -13,8 +27,8 @@
 			<?php } ?>
 			<p class="discount"><b><del><?php $data->discount_price ?></del></b></p>
 			<p class="quantity"><b>Q: <?= $data->quantity ?></b></p>
-		</div> -->
-		<!-- <form action="/Cart/index">
+		</div>
+		<form action="/Cart/index">
 			<button class="add">Add to cart</button>
 		</form>
 		<form action="/Product/details/<?= $data->product_id; ?>">
