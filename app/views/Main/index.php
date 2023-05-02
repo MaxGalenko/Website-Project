9c1166a4e8c707ee4f21a9898114b88708d7a163
Aliraza1002 <Aliraza_159@outlook.com>
2023-05-02 18:54:51 -0400

    small fixes in main page

--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -13,17 +13,4 @@ foreach ($data as $product) {
 
 </div>
 
-<!-- 
-	<div class="container mt-5" style="min-height: 400px;">
-		<div class="row">
-			<div class="col-md-4">
-				<div class="image">
-					<?php if($data->image){ ?>
-						<img src="/images/<?= $data->image ?>" alt="Product Image" style="height: 50%;">
-					<?php } ?>
-				</div>			
-			</div>
-		</div>
-	</div> -->
-
 <?php $this->view('shared/footer'); ?>
--- app/views/Product/mainPartial.php
+++ app/views/Product/mainPartial.php
@@ -1,5 +1,5 @@
 <main>
-	<div class="card">
+<!-- 	<div class="card">
 		<div class="image">
 			<?php if($data->image){ ?>
 				<img src="/images/<?= $data->image ?>" alt="Product Image">
@@ -8,9 +8,9 @@
 		<div class="caption">
 			<h6 class="product_name"><?= $data->title ?></h6>
 			<p class="price"><b><?= $data->unit_price ?></b></p>
-			<!-- <?php if ($data->discount_price != 0) { ?>
+			<?php if ($data->discount_price != 0) { ?>
 				<p class="discount"><b><del><?php $data->discount_price ?></del></b></p>
-			<?php } ?> -->
+			<?php } ?>
 			<p class="discount"><b><del><?php $data->discount_price ?></del></b></p>
 			<p class="quantity"><b>Q: <?= $data->quantity ?></b></p>
 		</div>
@@ -18,7 +18,7 @@
 			<button class="add">Add to cart</button>
 		</form>
 		 <a href="/Product/details/<?= $data->product_id; ?>"><button>View details</button></a>
-	</div>
+	</div> -->
 	<div class="card" style="width: 18rem;">
 	  <div class="image">
 			<?php if($data->image){ ?>
