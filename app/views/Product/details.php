0f7256f44c16698408c2f5086aac2c2c831216c0
Aliraza1002 <Aliraza_159@outlook.com>
2023-05-01 22:45:47 -0400

    product details page complete

--- app/views/Product/details.php
+++ app/views/Product/details.php
@@ -1,19 +1,23 @@
 <?php $this->view('shared/header', 'Product Details'); ?>
+
+<link rel="stylesheet" type="text/css" href="/css/MainStyle.css">
+
 <div class="container">
     <div class="card">
         <div class="card-body">
             <?php if ($data) { ?>
+            	<?php if ($data->image) { ?>
+                    <img src="/images/<?= $data->image ?>" width="50%" class="float-img">
+                <?php } ?>
+            <?php } else { ?>
+                <p>Product not found.</p>
+            <?php } ?>
                 <h1><?= $data->title ?></h1>
                 <p><b>Type:</b> <?= $data->type ?></p>
                 <p><b>Description:</b> <?= $data->description ?></p>
                 <p><b>Unit Price:</b> <?= $data->unit_price ?></p>
                 <p><b>Quantity:</b> <?= $data->quantity ?></p>
-                <?php if ($data->image) { ?>
-                    <img src="/images/<?= $data->image ?>" width="200">
-                <?php } ?>
-            <?php } else { ?>
-                <p>Product not found.</p>
-            <?php } ?>
+
             <br><a href="/Main/index" class="btn btn-secondary">Back</a>
         </div>
     </div>
