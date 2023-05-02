01e0084d2ca6fa5ce3d7f7df3a78a0fed7def082
Aliraza1002 <aliraza_159@outlook.com>
2023-05-02 11:46:47 -0400

    small changes to main page, still in progress

--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -8,15 +8,30 @@
 <div class="row row-cols-1 row-cols-md-5">
 <?php
 foreach ($data as $product) { ?>
-		<div class="card" style="width: 18rem;">
-	  <img src="..." class="card-img-top" alt="...">
+	<div class="card" style="width: 18rem;">
+	  <img src="..." class="card-img-top" alt="No picture for this product">
 	  <div class="card-body">
+	    <h5 class="card-title">Card title</h5>
 	    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+	    <a href="#" class="btn btn-primary">Go somewhere</a>
 	  </div>
 	</div>
 <?php
 }
 ?>
 </div>
+
+<!-- 
+	<div class="container mt-5" style="min-height: 400px;">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="image">
+					<?php if($data->image){ ?>
+						<img src="/images/<?= $data->image ?>" alt="Product Image" style="height: 50%;">
+					<?php } ?>
+				</div>			
+			</div>
+		</div>
+	</div> -->
 
 <?php $this->view('shared/footer'); ?>
