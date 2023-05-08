479145107dabb3b8cdb07837cf36672bd7902b43
Mert Kairstan Salvador <2179970@edu.vaniercollege.qc.ca>
2023-05-08 19:16:17 -0400

    added the filter | fixing the default

--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -1,11 +1,21 @@
 <?php $this->view('shared/header', 'Main Page'); ?>
-
+<script src="/json/filter.js"></script>
 <link rel="stylesheet" type="text/css" href="/css/Main/style.css">
 
 <h1 style="text-align: center;">Products</h1>
 
 <!-- Show product list -->
-<div class="row row-cols-1 row-cols-md-5">
+
+<form action="/Main/index" method="post">
+	<label for="filter">Filter</label>
+	<select name="filter" onchange="this.form.submit();" id="filter">
+    	<option value="default">Default</option>
+    	<option value="ascending">Ascending</option>
+    	<option value="descending">Descending</option>
+  	</select>
+</form>
+
+<div class="row row-cols-1 row-cols-md-5" id="prods">
 <?php
 foreach ($data as $product) {
 	$this->view('Product/mainPartial', $product);
