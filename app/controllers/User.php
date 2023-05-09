5c3894dfa6b69ba4586ae7e7c0693ceb2b856772
Mert Kairstan Salvador <2179970@edu.vaniercollege.qc.ca>
2023-05-09 17:27:52 -0400

    setup complete

--- app/controllers/User.php
+++ app/controllers/User.php
@@ -58,4 +58,43 @@ class User extends \app\core\Controller{
 		$messages = $message->getAllForUser($_SESSION['user_id']);
 		$this->view('User/profile',$messages);
 	}
+
+	//Adding the makeQRCode function
+	// Use: /Default/makeQRCode?data=protocol://address 
+	public function makeQRCode(){  
+		$data = $_GET['data'];  
+		\QRcode::png($data); 
+	}
+
+	//Previous 2fa authentication
+
+	// #[\app\filters\Login] 
+	// public function setup2fa(){     
+	// 	$secretkey = \app\core\TokenAuth6238::generateRandomClue();     
+	// 	$_SESSION['secretkey'] = $secretkey;     
+	// 	$url = \app\core\TokenAuth6238::getLocalCodeUrl( $_SESSION['user_id'], 'Awesome.com', $secretkey, 'Awesome App');     
+	// 	$this->view('User/twofasetup', $url); 
+	// } 
+
+	#[\app\filters\Login] public function setup2fa(){     
+		if(isset($_POST['action'])){         
+			$currentcode = $_POST['currentCode'];         
+			if(\app\core\TokenAuth6238::verify($_SESSION['secretkey'],$currentcode)){ 
+					//the user has verified their proper 2-factor authentication setup             
+					$user = new \app\models\User();             
+					$user->user_id = $_SESSION['user_id'];             
+					$user->secret_key = $_SESSION['secretkey'];             $
+					user->update2fa();             
+					header('location:/Somewhere***');         
+				}else{             
+					header('location:/User/setup2fa?error=token not verified!');
+					//reload         
+				}     
+			}else{         
+				$secretkey = \app\core\TokenAuth6238::generateRandomClue();         
+				$_SESSION['secretkey'] = $secretkey;         
+				$url = \app\core\TokenAuth6238::getLocalCodeUrl($_SESSION['username'], 'Awesome.com', $secretkey, 'Awesome App');         
+				$this->view('User/twofasetup', $url);     
+			} 
+	} 
 }
