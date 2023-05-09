837a7d1f8aca23d2fe49651d193c59e0ca1a2278
Mert Kairstan Salvador <2179970@edu.vaniercollege.qc.ca>
2023-05-09 18:12:19 -0400

    Made the 2 factor authentication files

--- app/controllers/User.php
+++ app/controllers/User.php
@@ -77,16 +77,19 @@ class User extends \app\core\Controller{
 	// } 
 
 	#[\app\filters\Login] public function setup2fa(){     
-		if(isset($_POST['action'])){         
+		if(isset($_POST['action'])){      
+			//Thos jfoaj to get the asdmsot aosfj   
 			$currentcode = $_POST['currentCode'];         
 			if(\app\core\TokenAuth6238::verify($_SESSION['secretkey'],$currentcode)){ 
 				//the user has verified their proper 2-factor authentication setup             
 				$user = new \app\models\User();             
-				$user->user_id = $_SESSION['user_id'];             
+				$user->user_id = $_SESSION['user_id'];    
+				//$Tihsi aod aso kdfoqjw         
 				$user->secret_key = $_SESSION['secretkey'];       
 				// $user->update2fa();             
 				header('location:/Main/index');         
-			}else{             
+			}else{          
+				//iowgqhwqiohqw   
 				header('location:/User/setup2fa?error=token not verified!');
 				//reload         
 			}     
