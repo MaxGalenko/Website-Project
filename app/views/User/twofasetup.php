3d817a90e9dc9f6647fdeec64fcb633e8100620c
Aliraza1002 <aliraza_159@outlook.com>
2023-05-10 09:30:55 -0400

    2FA fixes

--- app/views/User/twofasetup.php
+++ app/views/User/twofasetup.php
@@ -1,7 +1,6 @@
-<?php $this->view('shared/header', 'Create product'); ?>
+
 <img src="/User/makeQRCode?data=<?= $data ?>" /> 
     <form method="post" action="">  
         <label>Current code:<input type="text" name="currentCode" /></label>  
         <input type="submit" name="action" value="Verify code" /> 
     </form>
-<?php $this->view('shared/footer'); ?>
